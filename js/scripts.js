3547b7248ef04aa7c2a379998d45cd2926509238
Tim chadwick <battim3c@gmail.com>
2022-06-27 16:21:21 -0600

    round 1

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+/*!
+* Start Bootstrap - frontendmentor-product-preview v5.0.8 (https://github.com/battim/frontendmentor-product-preview)
+* Copyright 2013-2022 Tim Chadwick
+* Licensed under MIT (https://github.com/StartBootstrap/frontendmentor-product-preview/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
